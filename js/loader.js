13a0623ff0725bdc7251f179fc9c5b74c5d668df
Rod-76 <82039720+Rod-76@users.noreply.github.com>
2022-12-16 13:59:47 +0800

    test

--- js/loader.js
+++ js/loader.js
@@ -1,23 +1,33 @@
-$(document).ready(function(){
-    $('.modal').modal();
-    $('.collapsible').collapsible();
-  })
+$(document).ready(function () {
+  $('.modal').modal();
+  $('.collapsible').collapsible();
+  $('.datepicker').datepicker();
+  $('.sidenav').sidenav();
+  $('.dropdown-trigger').dropdown();
 
-function toggleModal() {
-    var instance = M.Modal.getInstance($('#modal3'));
-    instance.open();
-}
+})
 
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach((entry) => {
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
       console.log(entry)
-        if(entry.isIntersecting){
+      if (entry.isIntersecting) {
           entry.target.classList.add('show');
-        } else {
+      } else {
           entry.target.classList.remove('show');
-        }
-      });
+      }
+  });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+$(document).ready(function () {
+  $('.pushpin-demo-nav').each(function () {
+      var $this = $(this);
+      var $target = $('#' + $(this).attr('data-target'));
+      $this.pushpin({
+          top: $target.offset().top,
+          bottom: $target.offset().top + $target.outerHeight() - $this.height()
+      });
+  });
+});
